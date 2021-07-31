5ba6d4a866d90c5faaffb8c6d0b6de7f953be850
ManojBaasha <manoj.elango123@gmail.com>
2021-07-30 17:54:28 -0700

    seperation of content

--- main.py
+++ main.py
@@ -18,3 +18,18 @@ a = top_headlines["articles"]
 print(len(a))
 for i in a:
     print(i.keys())
+
+def news(top_headlines):
+    list_of_news_articles=top_headlines["articles"]
+    for i in list_of_news_articles:
+        source=i["source"]
+        author=i["author"]
+        title=i["title"]
+        description=i["description"]
+        url=i["url"]
+        urltoimage=i["urlToImage"]
+        publishedat=i["publishedAt"]
+        content=i["content"]
+        print(author,"\n\n\n", title,"\n\n\n", description,"\n\n\n", url,"\n\n\n", urltoimage,"\n\n\n", publishedat,"\n\n\n", content)
+
+news(top_headlines)
